53b1a85400799377459b63b8ba76eb323f649b0f
Ujjwal <itsujji@gmail.com>
2022-06-20 22:52:03 +0530

    Side-nav icons added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import {Admin, Resource} from 'react-admin'
+import {Admin, Resource, ListGuesser} from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
 import {TodoList} from './TodoList.js'
 import {EditList} from './EditList'
+import PostIcon from '@mui/icons-material/Book';
+import UserIcon from '@mui/icons-material/Group';
 
 function App() {
 
@@ -11,7 +13,8 @@ function App() {
   return (
     <div className="App">
       <Admin dataProvider={dataProvider}>
-        <Resource name="todos" list={TodoList} edit={EditList} />
+        <Resource name="todos" list={TodoList} edit={EditList}  icon={PostIcon} />
+        <Resource name="users" icon={UserIcon} list={ListGuesser} />
       </Admin>
     </div>
   );
